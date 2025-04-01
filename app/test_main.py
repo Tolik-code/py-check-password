4223e3f5c57ecdbf6c2446ca6a58ff233ac0bb22
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-04-01 17:34:51 +0300

    Added tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,38 @@
-# write your code here
+import pytest
+
+
+@pytest.mark.parametrize(
+    "password,expected",
+    [
+        pytest.param("dawdwaFdw&!", False, id="False if without numbers"),
+        pytest.param(
+            "123!4dwad5678",
+            False,
+            id="False if without uppercase"
+        ),
+        pytest.param(
+            "1234dWad5678",
+            False,
+            id="False if without special symbols"
+        ),
+        pytest.param(
+            "daw12dwaFdw&!",
+            True,
+            id="True if all conditions are met"
+        ),
+        pytest.param(
+            "dawdwaadwwadFdw&!dawdwaFdw&!",
+            False,
+            id="False if too long"
+        ),
+        pytest.param(
+            "D1w&!",
+            False,
+            id="False if too short"
+        ),
+    ]
+)
+def test_check_password(password: str, expected: bool) -> None:
+    from app.main import check_password
+
+    assert check_password(password) == expected
